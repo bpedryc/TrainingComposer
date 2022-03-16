f9c9e46f338e9efcb6a9c5a0d5af3c23b1d4d550
Bartłomiej Pedryc <b.pedryc11@gmail.com>
2022-03-16 12:17:57 +0100

    Update dependencies and set up Compose Destinations

--- app/src/main/java/com/haxos/trainingcomposer/MainActivity.kt
+++ app/src/main/java/com/haxos/trainingcomposer/MainActivity.kt
@@ -3,41 +3,56 @@ package com.haxos.trainingcomposer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.haxos.trainingcomposer.destinations.ComposeTrainingScreenDestination
 import com.haxos.trainingcomposer.ui.theme.TrainingComposerTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TrainingComposerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Android")
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
     }
 }
 
+@Destination(start = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(
+    navigator: DestinationsNavigator
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = { navigator.navigate(ComposeTrainingScreenDestination()) }
+        ) {
+            Text("Compose Training")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Destination
 @Composable
-fun DefaultPreview() {
-    TrainingComposerTheme {
-        Greeting("Android")
-    }
+fun ComposeTrainingScreen() {
+    Text("TEST")
 }
